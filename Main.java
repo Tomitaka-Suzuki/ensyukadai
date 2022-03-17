2a379f3d9ed671862e5f00ef0060219623c23e3e
Tomitaka Suzuki <t.suzuki.sys21@morijyobi.ac.jp>
2022-03-17 13:32:25 +0900

    Main.javaの内容変更

--- Main.java
+++ Main.java
@@ -2,13 +2,18 @@ import java.util.*;
 public class Main{
         public static void main(String[] args){
                 Scanner stdin = new Scanner(System.in);
-                double tall,weight,bmi;
-                tall = stdin.nextInt();
-                weight = stdin.nextInt();
-                bmi = 0.0;
-		tall = tall / 10;
-                bmi = weight / ( tall * tall);
-                System.out.println("BMI値は" + bmi);
+                double tall,weight,bmi,cnt;
+		cnt = 0;
+		while( cnt < 2 ){
+                	tall = stdin.nextInt();
+                	weight = stdin.nextInt();
+                	bmi = 0.0;
+			tall = tall / 100;
+                	bmi = weight / ( tall * tall);
+			bmi = ((double)Math.round(bmi * 10))/10;
+                	System.out.println("BMI値は" + bmi);
+			cnt = cnt + 1;
+		}
         }
 }
 
